562f87a2cf80cc23dae0a2aa40b34966252647bf
Oscar Lauth <oscar.lauth@gmail.com>
2023-03-25 16:44:02 -0400

    Add Question class

--- creditscoresim/src/App.jsx
+++ creditscoresim/src/App.jsx
@@ -1,11 +1,21 @@
 import './styles/style.css';
 import QuestionCard from './QuestionCard';
 
-questions = []
+
+
+class Question {
+    constructor(questionText, options, nextQuestion) {
+        this.questionText = questionText;
+        this.options = options;
+        this.nextQuestion = nextQuestion;
+    }
+}
+
+questions = [Question("Question 1",["A","B"],Question("Question 2",["#","?"],Question("Question 3",["Hi","Bye"],null)))]
 
 function App() {
   return (
-    
+    <div></div>
     <QuestionCard questionText={"Question 1"} options={["Option A","Option B"]}/>
   );
 }
