a47fc5cd42cd7c64db5a8864bed172746533841b
Oscar Lauth <oscar.lauth@gmail.com>
2023-03-26 00:31:02 -0400

    applyeffects func

--- creditscoresim/src/OptionBtn.jsx
+++ creditscoresim/src/OptionBtn.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 // import QuestionCard from './QuestionCard'
 
 
-const OptionBtn = ({ o, nq}) => {
+const OptionBtn = ({ o, nq, p}) => {
+    const applyEffects = ()=> {
+        p.col += o.colEffect;
+        p.atp += o.atpEffect;
+        p.debts += o.debtEffect;
+        p.age+=o.ageEffect;
+        p.ph += o.phEffect;
+        
+        let netIncome = p.atp - p.col;
+        p.ph += netIncome*.2;
+        p.debts -= netIncome*.5;
+        if (p.debts < 0) p.debts = 0;
+        // p.growth += p.growthEffect;
 
+    }
   return (
     <div>
-        <button className='option-btn' onClick={()=>{nq(o)}}>
+        <button className='option-btn' onClick={()=>{nq(o);applyEffects()}}>
             {o.optionText}
         </button>
     </div>
