d5f048f6f11bd1d4d78aa3c8712845bbc28ae0bb
Grant Sweeney <grantsweeney02@gmail.com>
2023-03-26 01:57:47 -0400

    finished css and html for question and final page

--- creditscoresim/src/pages/Home.jsx
+++ creditscoresim/src/pages/Home.jsx
@@ -3,14 +3,19 @@ import "../styles/style.css";
 const Home = () => {
     return (
         <body>
-            <div className="question__card">
-                <h3 className="question__text">
-                    What type of car would you buy
-                </h3>
-                {/* <div className='answer-section' onChange={(e)=>{onSelectAnswer(e)}}> */}
-                <div className="question__options">
-                    <button className="question__options_option">NEW</button>
-                    <button className="question__options_option">OLD</button>
+            <div className="container">
+                <div className="question__card">
+                    <h3 className="question__text">You Finished!</h3>
+                    <div class="line"></div>
+                    {/* <div className='answer-section' onChange={(e)=>{onSelectAnswer(e)}}> */}
+                    <div className="question__options">
+                        <button className="question__options_option">
+                            Play Again?
+                        </button>
+                    </div>
+                </div>
+                <div className="credit_score">
+                    <h1 className="credit_score_text">Credit Score: </h1>
                 </div>
             </div>
         </body>
