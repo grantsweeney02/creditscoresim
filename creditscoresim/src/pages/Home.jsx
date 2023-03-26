e2b542d0f352268a545e90a4111c882e660347b5
Grant Sweeney <grantsweeney02@gmail.com>
2023-03-26 04:55:08 -0400

    added sign and octocat

--- creditscoresim/src/pages/Home.jsx
+++ creditscoresim/src/pages/Home.jsx
@@ -3,8 +3,13 @@ import "../styles/style.css";
 const Home = () => {
     return (
         <body>
-            <div className="container__home" style={""}>
-                <a href="/sim">PLAY NOW</a>
+            <div className="container__home">
+                <div className="sign">
+                    <a class="sign__text" href="/sim">
+                        PLAY NOW
+                    </a>
+                    <img src="/images/pointer_cat.png" />
+                </div>
             </div>
         </body>
     );
