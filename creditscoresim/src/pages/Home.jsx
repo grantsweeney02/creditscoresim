f376718e0775abd411d4ddb6e4fbafaf678f82a3
Grant Sweeney <grantsweeney02@gmail.com>
2023-03-25 23:51:49 -0400

    starting the question cards

--- creditscoresim/src/pages/Home.jsx
+++ creditscoresim/src/pages/Home.jsx
@@ -3,7 +3,16 @@ import "../styles/style.css";
 const Home = () => {
     return (
         <body>
-            <img class="pirate_ship" src="../../images/pirate_ship.png" alt="" />
+            <div className="question__card">
+                <h3 className="question__text">
+                    What type of car would you buy
+                </h3>
+                {/* <div className='answer-section' onChange={(e)=>{onSelectAnswer(e)}}> */}
+                <div className="question__options">
+                    <button className="question__options_option">NEW</button>
+                    <button className="question__options_option">OLD</button>
+                </div>
+            </div>
         </body>
     );
 };
