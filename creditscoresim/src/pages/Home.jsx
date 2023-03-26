b06852b001feec5256a08ca9482de79e994c588f
Grant Sweeney <grantsweeney02@gmail.com>
2023-03-26 04:20:25 -0400

    adding new starter image

--- creditscoresim/src/pages/Home.jsx
+++ creditscoresim/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import "../styles/style.css";
 const Home = () => {
     return (
         <body>
-            <div className="container__home">HELLO WORLD</div>
+            <div className="container__home" style={""}>
+                <a href="/sim">PLAY NOW</a>
+            </div>
         </body>
     );
 };
